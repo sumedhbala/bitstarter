98cfec6f3a47764db426614486d641fef19709c2
sumedh <sumedhbala@gmail.com>
2013-07-12 01:03:36 +0000

    Ateempt to read from file

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
 
+var fs = require('fs');
+
+var buffer = new Buffer (20);
+
 var app = express.createServer(express.logger());
 
+buffer = fs.readFileSync('./index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
